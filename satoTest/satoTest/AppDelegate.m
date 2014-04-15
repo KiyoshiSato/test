50ad30c394f30bc4258cdbe1563b993aece7a2e4
佐藤 圭 <KiyoshiSato@satou-no-MacBook-Air.local>
2014-04-15 12:50:02 +0900

    no message

--- satoTest/satoTest/AppDelegate.m
+++ satoTest/satoTest/AppDelegate.m
@@ -12,6 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    /////////////ama,gfnalnglangfla
     // Override point for customization after application launch.
     return YES;
 }
